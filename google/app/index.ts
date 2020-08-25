cbc919132862dbdbd40e4bdd638971c4854d3029
grant100 <gsowards100@gmail.com>
2020-08-24 21:03:03 -0600

    app/index.ts: update ip

--- google/app/index.ts
+++ google/app/index.ts
@@ -1,7 +1,7 @@
 /// <reference types="@google/local-home-sdk" />
 const axios = require('axios').default;
 const apiClient = axios.create({
-  baseURL: 'http://10.0.0.173:8081',
+  baseURL: 'http://192.168.1.173:8081',
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json'

30294aafe583ee5bbadac614fb141c888aa947d6
grant100 <gsowards100@gmail.com>
2020-08-31 21:22:01 -0600

    app/index.ts: fix ts

--- google/app/index.ts
+++ google/app/index.ts
@@ -1,4 +1,5 @@
 /// <reference types="@google/local-home-sdk" />
+
 const axios = require('axios').default;
 const apiClient = axios.create({
   baseURL: 'http://192.168.1.173:8081',
@@ -8,11 +9,11 @@ const apiClient = axios.create({
   }
 });
 
-const app = new smarthome.App("1.0.0")
-  .onIdentify((request) => {
+const app: any = new smarthome.App("1.0.0")
+  .onIdentify((request: any) => {
     console.debug("IDENTIFY request:", request);
 
-    const device = request.inputs[0].payload.device;
+    const device: any = request.inputs[0].payload.device;
 
     return new Promise((resolve, reject) => {
       const response = {
@@ -28,7 +29,7 @@ const app = new smarthome.App("1.0.0")
       resolve(response);
     });
   })
-  .onExecute((request) => {
+  .onExecute((request: any) => {
     console.debug("EXECUTE request", request);
 
     const response = new smarthome.Execute.Response.Builder()
@@ -38,7 +39,7 @@ const app = new smarthome.App("1.0.0")
     const params = execution.params;
     console.log("command", command);
         
-    const result =  command.devices.map((device) => {
+    const result =  command.devices.map((device: any) => {
       const states = {
         status: 'SUCCESS',
         state:{on: true,
@@ -46,7 +47,7 @@ const app = new smarthome.App("1.0.0")
         
       };
       const params = new URLSearchParams();
-      return apiClient.post('/action', execution)
+      return apiClient.post('/action', {'command': execution, 'requestId': request.requestId})
         .then(function (hresponse : any) {
           // handle success
           console.log(hresponse);
